5958f277f6e66a58432fd8a24b704ce4072ee971
joaopaludo <joaopgpaludo@gmail.com>
2023-08-06 22:14:14 -0300

    Doc: 1037 - Java

---
+++ 2023/bee1037/Main.java
@@ -0,0 +1,27 @@
+package BEE1037;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		double valor = teclado.nextDouble();
+		
+		if (valor < 0) {
+			System.out.println("Fora de intervalo");
+		} else if (valor <= 25) {
+			System.out.println("Intervalo [0,25]");
+		} else if (valor <= 50) {
+			System.out.println("Intervalo (25,50]");
+		} else if (valor <= 75) {
+			System.out.println("Intervalo (50,75]");
+		} else if (valor <= 100) {
+			System.out.println("Intervalo (75,100]");
+		} else {
+			System.out.println("Fora de intervalo");
+		}
+		
+		teclado.close();
+	}
+}
